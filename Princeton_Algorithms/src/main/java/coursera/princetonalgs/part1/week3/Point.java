38024140382810509542dfa3cba4274b49bc916d
alex <alexander.zakshevsky@itc-engineering.com>
2018-01-10 18:11:58 +0100

    reused Point class from the task description

--- Princeton_Algorithms/src/main/java/coursera/princetonalgs/part1/week3/Point.java
+++ Princeton_Algorithms/src/main/java/coursera/princetonalgs/part1/week3/Point.java
@@ -7,54 +7,54 @@ import java.util.Objects;
 
 public class Point implements Comparable<Point> {
 
-    private final int x;
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
 
-    private final int y;
-
+    /**
+     * Initializes a new point.
+     *
+     * @param  x the <em>x</em>-coordinate of the point
+     * @param  y the <em>y</em>-coordinate of the point
+     */
     public Point(int x, int y) {
+        /* DO NOT MODIFY */
         this.x = x;
         this.y = y;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Point point = (Point) obj;
-        return x == point.x &&
-                y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
+    /**
+     * Draws this point to standard draw.
+     */
     public void draw() {
-        StdDraw.circle(x, y, 1d);
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
     }
 
-    public void drawTo(Point point) {
-        StdDraw.line(this.x, this.y, point.x, point.y);
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
-    @Override
-    public String toString() {
-        return "(" + x + ", " + y + ")";
-    }
-
-    @Override
-    public int compareTo(Point that) {
-        int result = Integer.compare(that.y, this.y);
-        if (result == 0) {
-            return Integer.compare(that.x, this.x);
-        }
-        else {
-            return result;
-        }
-    }
-
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
+     */
     public double slopeTo(Point that) {
+        /* YOUR CODE HERE */
+
         int dy = that.y - this.y;
         int dx = that.x - this.x;
 
@@ -71,7 +71,54 @@ public class Point implements Comparable<Point> {
         }
     }
 
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+
+        int result = Integer.compare(that.y, this.y);
+        if (result == 0) {
+            return Integer.compare(that.x, this.x);
+        }
+        else {
+            return result;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point point = (Point) obj;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(x, y);
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
     public Comparator<Point> slopeOrder() {
+        /* YOUR CODE HERE */
+
         return new Comparator<Point>() {
             @Override
             public int compare(Point o1, Point o2) {
@@ -84,5 +131,25 @@ public class Point implements Comparable<Point> {
                 }
             }
         };
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
     }
 }
